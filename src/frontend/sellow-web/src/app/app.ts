f7cc9d3e76042fae4ffef8894c43a912eafbdc6e
Kuba <k.szkowrytko@gmail.com>
2026-09-22 15:50:31 +0200

    chore: set up backend and frontend application skeleton (#5)
    * chore: set up backend and frontend application skeleton
    
    * chore: clean up application skeleton after review

---
+++ src/frontend/sellow-web/src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { HlmButtonImports } from '@spartan-ng/helm/button'
+
+@Component({
+  imports: [HlmButtonImports],
+  selector: 'app-root',
+  styleUrl: './app.css',
+  templateUrl: './app.html',
+})
+export class App {
+}
